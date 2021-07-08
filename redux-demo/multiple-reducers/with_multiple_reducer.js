c7cb25a43131c896aca3fc65b51a6de05b3b720c
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-07-08 11:56:19 +0530

    multiple reducer

--- redux-demo/multiple-reducers/with_multiple_reducer.js
+++ redux-demo/multiple-reducers/with_multiple_reducer.js
@@ -1,6 +1,74 @@
 const redux = require('redux');
 const createStore = redux.createStore;
 
+const combineReducers = redux.combineReducers;
+
+const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
+
+// action creator
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
+
+function buyIceCream() {
+    return {
+        type: BUY_ICECREAM,
+    }
+}
+
+const initialState = {
+    numOfCakes: 10,
+    numOfIceCream: 20
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_CAKE:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1
+            }
+        
+        case BUY_ICECREAM:
+            return {
+                ...state,
+                numOfIceCream: state.numOfIceCream - 1
+            }
+    
+        default:
+            return state;
+    }
+}
+
+// combine multiple reducer
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+})
+const store = createStore(rootReducer);
+
+
+console.log('initial state', store.getState());
+
+const unsubscribe = store.subscribe(() => console.log('updated state', store.getState()))
+
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+
+unsubscribe();
+
+
+const redux = require('redux');
+const createStore = redux.createStore;
+
 const BUY_CAKE = 'BUY_CAKE';
 const BUY_ICECREAM = 'BUY_ICECREAM';
 
@@ -35,7 +103,7 @@ const cakeReducer = (state = initialCakeState, action) => {
                 ...state,
                 numOfCakes: state.numOfCakes - 1
             }
-
+    
         default:
             return state;
     }
