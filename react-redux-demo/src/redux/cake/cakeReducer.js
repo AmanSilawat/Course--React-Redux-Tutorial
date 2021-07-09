c1e51fb55000cd4d6ddbd2b68a6c11d0d05c5629
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-07-09 12:14:38 +0530

    redux devtool extension and action paylod

--- react-redux-demo/src/redux/cake/cakeReducer.js
+++ react-redux-demo/src/redux/cake/cakeReducer.js
@@ -9,7 +9,7 @@ const reducer = (state = initialSate, action) => {
         case BUY_CAKE:
             return {
                 ...state,
-                numOfCakes: state.numOfCakes - 1
+                numOfCakes: state.numOfCakes - action.payload
             }
     
         default:
