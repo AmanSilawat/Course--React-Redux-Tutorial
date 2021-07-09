de87e783cc5515aeeb043a601bbf2bba7992edc8
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-07-09 12:35:31 +0530

    own props in mapStateToProps

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -5,11 +5,18 @@ import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        
+        {/* own props in mapStateToProps */}
+        <ItemContainer cake />
+        <ItemContainer />
+
+
         <CakeContainer />
         <HooksCakeContainer />
         <IceCreamContainer />
